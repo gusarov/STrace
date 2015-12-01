e661ddfff7a92b2c809686d52a2d0c3add44b130
Dmitry Gusarov <gitrepo@xk.33m.co>
2015-12-01 13:46:32 +0300

    first version

--- STrace.cpp
+++ STrace.cpp
@@ -3,48 +3,133 @@
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
+
 #if ARDUINO >= 100
  #include "Arduino.h"
 #else
  #include "WProgram.h"
 #endif
 
-class STrace {
-public:
-  void write(byte level, String message);
-  void debug(String message);
-  void info(String message);
-  void important(String message);
-  void warning(String message);
-  void error(String message);
-};
+int lenDigsQuick(int value){
+	if (value >= 1000) return 4;
+	if (value >= 100) return 3;
+	if (value >= 10) return 2;
+	return 1;
+}
 
+int lenDigs(int value){
+	int l = 0;
+
+	do {
+		l++;
+		value /= 10;
+	} while (value > 0);
+	
+	return l;
+}
+
+void printPadding(int value, int padding){
+	int l = lenDigsQuick(value);
+	for(int i = l; i < padding; i++) {
+		Serial.print(' ');
+	}
+	Serial.print(value);
+}
+
+char getLevel(byte level) {
+	switch(level) {
+		case STRACE_VERBOSE:
+			return 'V';
+		case STRACE_DEBUG:
+			return 'D';
+		case STRACE_INFO:
+			return 'I';
+		case STRACE_IMPORTANT:
+			return 'M';
+		case STRACE_WARNING:
+			return 'W';
+		case STRACE_ERROR:
+			return 'E';
+	}
+}
+
+void write2(byte level, String tag, String message)
+{
+	printPadding(millis() / 1000, 3);
+	Serial.print(": ");
+	Serial.print(getLevel(level));
+	Serial.print('/');
+	Serial.print(tag);
+	Serial.print(": ");
+	Serial.println(message);
+}
+
+STrace::STrace(String tag)
+{
+	_tag = tag;
+}
 
 void STrace::write(byte level, String message)
 {
+	write2(level, _tag, message);
 }
 
-void STrace::debug(String message)
+void STrace::v(String message)
+{
+	STrace::write(STRACE_VERBOSE, message);
+}
+
+void STrace::d(String message)
 {
 	STrace::write(STRACE_DEBUG, message);
 }
 
-void STrace::info(String message)
+void STrace::i(String message)
 {
 	STrace::write(STRACE_INFO, message);
 }
 
-void STrace::important(String message)
+void STrace::p(String message)
 {
 	STrace::write(STRACE_IMPORTANT, message);
 }
 
-void STrace::warning(String message)
+void STrace::w(String message)
 {
 	STrace::write(STRACE_WARNING, message);
 }
 
-void STrace::error(String message)
+void STrace::e(String message)
 {
 	STrace::write(STRACE_ERROR, message);
+}
+
+void logV(String code, String message)
+{
+	write2(STRACE_VERBOSE, code, message);
+}
+
+void logD(String code, String message)
+{
+	write2(STRACE_DEBUG, code, message);
+}
+
+void logI(String code, String message)
+{
+	write2(STRACE_INFO, code, message);
+}
+
+void logP(String code, String message)
+{
+	write2(STRACE_IMPORTANT, code, message);
+}
+
+void logW(String code, String message)
+{
+	write2(STRACE_WARNING, code, message);
+}
+
+void logE(String code, String message)
+{
+	write2(STRACE_ERROR, code, message);
 }
--- STrace.h
+++ STrace.h
@@ -1,20 +1,51 @@
 #ifndef STrace_h
 #define STrace_h
 
+#define STRACE_VERBOSE 0x05
 #define STRACE_DEBUG 0x10
 #define STRACE_INFO 0x20
 #define STRACE_IMPORTANT 0x30
 #define STRACE_WARNING 0x40
 #define STRACE_ERROR 0x50
 
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <stdarg.h>
+
+//void p(char* fmt, ... );
+//int lenDigs(int value);
+
+void write2(byte level, String code, String message);
+
+void logV(String code, String message);
+void logD(String code, String message);
+void logI(String code, String message);
+void logP(String code, String message);
+void logW(String code, String message);
+void logE(String code, String message);
+
 class STrace {
 public:
+  STrace(String tag);
+  void v(String message);
+  void d(String message);
+  void i(String message);
+  void p(String message);
+  void w(String message);
+  void e(String message);
+  
+private:
   void write(byte level, String message);
-  void debug(String message);
-  void info(String message);
-  void important(String message);
-  void warning(String message);
-  void error(String message);
+  String _tag;
 };
+
 
 #endif
